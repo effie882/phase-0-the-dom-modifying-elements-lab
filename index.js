fbc151e12bfadcf1a1d5003d8a5e2fbada47511f
effie882 <effiemorara@gmail.com>
2025-06-29 13:11:12 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Effie Morara is the champion";
+
+
+document.body.append(newHeader);
